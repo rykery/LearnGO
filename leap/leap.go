e74cf94312af480e0fb044544bf541372514e772
Ryker Youd <rykeryoud@gmail.com>
2024-07-24 10:48:24 -0400

    simplify leap calculations

--- leap/leap.go
+++ leap/leap.go
@@ -3,14 +3,8 @@ package leap
 
 // IsLeapYear calculates if a given year is a leap year
 func IsLeapYear(year int) bool {
-	if year%4 == 0 {
-		if year%100 == 0 {
-			if year%400 == 0 {
-				return true
-			}
-			return false
-		}
-		return true
+	if year%100 == 0 {
+		return year%400 == 0
 	}
-	return false
+	return year%4 == 0
 }
