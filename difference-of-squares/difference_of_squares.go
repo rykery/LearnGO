de3a4b870384e9145ecc58888272b9d60b587003
Ryker Youd <rykeryoud@gmail.com>
2024-07-28 15:57:52 -0400

    use infinite series simplification to optimize difference of squares

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,19 +1,12 @@
 package diffsquares
 
 func SquareOfSum(n int) int {
-	sum := 0
-	for i := 0; i <= n; i++ {
-		sum += i
-	}
+	sum := (n * (n + 1)) / 2
 	return sum * sum
 }
 
 func SumOfSquares(n int) int {
-	sum := 0
-	for i := 0; i <= n; i++ {
-		sum += i * i
-	}
-	return sum
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 func Difference(n int) int {
